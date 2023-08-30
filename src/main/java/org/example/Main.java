79d711c496b373cb7edc04677fb0f4f0838adde2
Nikolay <dojazz@icloud.com>
2023-08-30 14:18:06 +0300

    init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( "Введите имя, или целое число, или последовательность целых чисел разделенных запятой" );
+        Scanner scanner = new Scanner( System.in );
+        String input = scanner.nextLine();
+        System.out.println( output( input ) );
+    }
+
+    public static String output(String input) {
+
+        String[] data = input.split( "," );
+
+        if (data.length < 2 & isDigit( data[0] )) {
+
+            int a = Integer.parseInt( data[0] );
+            if (a > 7) {
+                return "Привет";
+            }
+            return "Поздароваюсь если ваше число больше 7";
+        }
+        if (data.length < 2) {
+            String s = "Вячеслав";
+
+            if (s.equalsIgnoreCase( data[0] )) {
+
+                return "Привет Вячеслав";
+            }
+            return "Нет такого имени";
+        }
+        if (data.length > 1 & isDigit( data[0] )) {
+
+            System.out.println("Вот числа кратные 3 из вашей последовательности");
+
+            int[] array = new int[data.length];
+            for (int i = 0; i < data.length; i++) {
+                array[i] = Integer.parseInt( data[i] );
+                if (array[i] % 3 == 0)
+                    System.out.println( array[i] );
+            }
+
+        }
+        return "Нет числа кратного 3";
+    }
+
+    private static boolean isDigit(String input) {
+        try {
+            Integer.parseInt( input );
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
